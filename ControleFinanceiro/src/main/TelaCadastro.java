25bf0ccb4827761a18d52f6e5af4d0df863fb590
romenich <romenik.chaves@hotmail.com>
2023-08-17 13:07:09 -0300

    Alterações adicionadas ao projeto

--- ControleFinanceiro/src/main/TelaCadastro.java
+++ ControleFinanceiro/src/main/TelaCadastro.java
@@ -144,6 +144,7 @@ public class TelaCadastro extends javax.swing.JFrame {
         );
 
         pack();
+        setLocationRelativeTo(null);
     }// </editor-fold>//GEN-END:initComponents
 
     private void voltarBTNActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_voltarBTNActionPerformed
